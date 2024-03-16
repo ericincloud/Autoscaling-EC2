6b587cf401b064aaa3c6a4a4fa4e09a59a0dd997
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-03-16 14:51:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,11 @@
-provider "aws" {
-  region = "us-east-1"  
-}
-
-resource "aws_vpc" "main" {  
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-}
-
-resource "aws_subnet" "public" {
+resource "aws_subnet" "private" {
   count                   = 3
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  map_public_ip_on_launch = true
+  cidr_block              = "10.0.${count.index + 3}.0/24"
+  map_public_ip_on_launch = false
 }
 
-resource "aws_route_table_association" "public" {
-  count          = 3
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_subnet" "private" {
-  count      = 3
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.${count.index + 3}.0/24"
-}
-
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -59,96 +25,26 @@ resource "aws_security_group" "sg" {
 
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
 }
 
 resource "aws_lb" "alb" {
   name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg.id]
-  subnets            = [aws_subnet.public[0].id, aws_subnet.private[0].id]
-}
-
-resource "aws_lb_target_group" "tg" {
-  name     = "my-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
-
-resource "aws_launch_configuration" "lc" {
-  name          = "my-lc"
-  image_id      = "ami-04479e961467114dd"
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.ec2_sg.id]
-}
-
-resource "aws_autoscaling_group" "asg" {
-  desired_capacity     = 1
-  max_size             = 5
-  min_size             = 1
-  health_check_type    = "ELB"
-  health_check_grace_period = 300
-  launch_configuration = aws_launch_configuration.lc.name
-  vpc_zone_identifier  = [aws_subnet.private[0].id, aws_subnet.public[0].id, aws_subnet.public[1].id]
-  target_group_arns    = [aws_lb_target_group.tg.arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "high_cpu_usage"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "70"
-  alarm_description   = "This metric triggers when CPU usage exceeds 70%"
-  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.asg.name
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "low_cpu" {
-  alarm_name          = "low_cpu_usage"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "40"
-  alarm_description   = "This metric triggers when CPU usage falls below 40%"
-  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.asg.name
-  }
-}
-
-resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "scale_up"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-}
-
-resource "aws_autoscaling_policy" "scale_down" {
-  name                   = "scale_down"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public[0].id, aws_subnet.public[1].id, aws_subnet.private[0].id]
 }
